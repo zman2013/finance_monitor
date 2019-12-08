84178a0a5a31e5ada6fc2d5e6a09f9c690a44bea
zman <manzhiyuan@chehejia.com>
2019-12-08 23:05:07 +0800

    delete setting.py

--- setting.py
+++
@@ -1,7 +0,0 @@
-# _*_ coding: utf-8 _*_
-
-import tushare as ts
-
-root_dir = '/Users/zman/PycharmProjects/data/stock/'
-
-ts_api = ts.pro_api('927125f3c18f54a3f91fd7e74e54ddc10c9b1b4c2a2da851b9cc44d0')
